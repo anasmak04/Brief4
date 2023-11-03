5ccf925e0ca22610f63b37dca00b61048d3c2e2a
Mehdi <elmahdi20erraji@gmail.com>
2023-11-03 10:36:39 +0100

    links are created and well guided

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,6 @@
 	});
 
 	
-	//  SubMenu Dropdown Toggle
-	if ($('.tp-main-menu nav > ul > li.has-dropdown > a').length) {
-		$('.tp-main-menu nav > ul > li.has-dropdown > a').append('<i class="fal fa-angle-down"></i>');
-	}
-
-  
 	//  wow animation
 	var wow = new WOW(
 		{
@@ -37,22 +31,6 @@
 	});
 
 
-	//. Mobile Menu Js
-	$('#mobile-menu').meanmenu({
-		meanMenuContainer: '.mobile-menu',
-		meanScreenWidth: "1199",
-		meanExpand: ['<i class="fal fa-plus"></i>'],
-	});
-	
-	//  Mobile Menu Js
-	$('#mobile-menu-2').meanmenu({
-		meanMenuContainer: '.mobile-menu',
-		meanScreenWidth: "6000",
-		meanExpand: ['<i class="fal fa-plus"></i>'],
-	});
-
-
-	
 $(window).on('load', function () {
 
 	$('#preloader').delay(350).fadeOut('slow');
@@ -62,13 +40,7 @@ $(window).on('load', function () {
 })
 	
 
-		//for menu active class
-		$('.masonary-menu button').on('click', function (event) {
-			$(this).siblings('.active').removeClass('active');
-			$(this).addClass('active');
-			event.preventDefault();
-		});
-
+	
 
 
 
